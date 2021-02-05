692fcda53fb128a6f33864f9271ce6c3aba115e7
Battulga <battulga.dev@gmail.com>
2021-02-05 10:49:37 +0800

    [REM] lab1.py

--- lab1.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('./data.txt', 'r') as f, open('./result.txt', 'w+') as out:
-    data = f.read()
-   # result = list(re.findall(r'[а-яА-Я]+ | \n | \d*', data))
-    result = re.sub(r'<.*>', '', data)
-    out.write(result.strip())
